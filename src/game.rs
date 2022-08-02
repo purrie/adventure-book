6a5a0003e7e43a54a9b2908dab2660845366e459
Purrie <purriestarshine@gmail.com>
2022-08-02 21:55:56 +0200

    Adding more event handlers

--- src/game.rs
+++ src/game.rs
@@ -1,20 +1,30 @@
-use fltk::app::{self, App, Receiver};
+use fltk::{
+    app::{self, App, Receiver},
+    draw::Rect,
+};
 
-use crate::{adventure::Adventure, file::capture_adventures, window::MainWindow};
+use crate::{
+    adventure::{Adventure, Page},
+    file::{capture_adventures, read_page},
+    window::MainWindow,
+};
 
 pub struct Game {
     app: App,
     window: MainWindow,
     adventures: Vec<Adventure>,
     receiver: Receiver<Event>,
-    active_adventure: usize,
+    selected_adventure: usize,
+    active_storybook: Adventure,
+    active_page: Page,
 }
 
 impl Game {
     pub fn create() -> Self {
         let app = App::default();
         let adventures = capture_adventures();
-        let window = MainWindow::create();
+        let window_size = Rect::new(0, 0, 1000, 750);
+        let window = MainWindow::create(window_size, window_size);
         let (_s, receiver) = app::channel();
 
         Game {
@@ -22,7 +32,9 @@ impl Game {
             adventures,
             window,
             receiver,
-            active_adventure: 0,
+            selected_adventure: 0,
+            active_storybook: Adventure::new(),
+            active_page: Page::new(),
         }
     }
     pub fn start(mut self) {
@@ -30,33 +42,60 @@ impl Game {
             if let Some(msg) = self.receiver.recv() {
                 match msg {
                     Event::DisplayAdventureSelect => {
-                        self.window.fill_adventure_choices(&self.adventures);
+                        self.window.main_menu.fill_adventure_choices(&self.adventures);
                         self.window.switch_to_adventure_choice();
-                    },
+                    }
                     Event::DisplayMainMenu => self.window.switch_to_main_menu(),
-                    Event::StartAdventure => {}
+                    Event::StartAdventure => {
+                        self.active_storybook = self.adventures[self.selected_adventure].clone();
+                        let page = self.active_storybook.start.clone();
+                        self.switch_to_page(&page);
+                        self.window.switch_to_game();
+                    }
                     Event::Quit => {
                         app::quit();
                     }
                     Event::SelectAdventure(txt) => {
                         if let Some(find) = self.adventures.iter().position(|x| x.title == txt) {
-                            self.active_adventure = find;
+                            self.selected_adventure = find;
                             let adventure = &self.adventures[find];
-                            self.window.set_adventure_choice(adventure);
+                            self.window.main_menu.set_adventure_preview_text(adventure);
                         }
                     }
                     Event::StoryChoice(index) => {
-
+                        let choice = &self.active_page.choices[index];
                     }
                     Event::QuitToMainMenu => {
-
+                        self.window.switch_to_adventure_choice();
                     }
                 }
             }
         }
     }
-    fn get_active_adventure(&self) -> &Adventure {
-        &self.adventures[self.active_adventure]
+    /// Changes currently displayed page.
+    ///
+    /// It refreshes windows contents to update changes in records and fills story and choices
+    fn switch_to_page(&mut self, page_name: &String) {
+        if let Ok(page) = read_page(&self.active_storybook.path, page_name) {
+            self.active_page = page;
+        } else {
+            // TODO panic for now, replace with proper error handling later
+            panic!(
+                "Adventure {} or its starting page is corrupted!",
+                self.active_storybook.title
+            );
+        }
+        let story = self.parse_story_text();
+        let choices = self.parse_choices();
+        self.window.game_window.fill_choices(choices);
+        self.window.game_window.display_story(story);
+        self.window.game_window.update_records(&self.active_storybook.records);
+    }
+    fn parse_story_text(&self) -> String {
+        unimplemented!();
+    }
+    fn parse_choices(&self) -> Vec<(bool, String)> {
+        unimplemented!();
     }
 }
 
