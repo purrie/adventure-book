2774ed87fac456cb176f8845e83bf017732218d1
Purrie <purriestarshine@gmail.com>
2022-09-16 18:28:50 +0200

    Dialog improvements
    Fixed issue where closing dialogs would be treated as accepting.
    Fixed dialog scaling to label length.
    Added a confirmation dialog.

--- src/dialog.rs
+++ src/dialog.rs
@@ -1,3 +1,5 @@
+use std::{rc::Rc, cell::RefCell};
+
 use fltk::{
     app,
     button::Button,
@@ -61,27 +63,31 @@ pub fn ask_to_choose_adventure(adventures: &Vec<Adventure>) -> Option<usize> {
 }
 
 pub fn ask_for_text(label: &str) -> Option<String> {
-    let mut win = Window::default().with_size(300, 150).with_label(label);
+    let len = i32::max(fltk::draw::width(label) as i32 + 20, 300);
 
-    Frame::new(50, 10, 200, 20, None).with_label(label);
-    let input = Input::new(50, 30, 200, 30, None);
+    let mut win = Window::default().with_size(len, 110).with_label(label);
 
-    let mut butt_accept = Button::new(210, 110, 80, 30, "Accept");
-    let mut butt_cancel = Button::new(10, 110, 80, 30, "Cancel");
+    Frame::new(20, 10, len - 40, 20, None).with_label(label);
+    let input = Input::new(20, 30, len - 40, 30, None);
+
+    let mut butt_accept = Button::new(len - 90, 70, 80, 30, "Accept");
+    let mut butt_cancel = Button::new(10, 70, 80, 30, "Cancel");
 
     win.end();
     win.make_modal(true);
     win.show();
 
+    let accept = Rc::new(RefCell::new(false));
+
     butt_accept.set_callback({
-        |x| {
+        let accept = Rc::clone(&accept);
+        move |x| {
+            *accept.borrow_mut() = true;
             x.window().unwrap().hide();
         }
     });
     butt_cancel.set_callback({
-        let mut input = input.clone();
-        move |x| {
-            input.set_value("");
+        |x| {
             x.window().unwrap().hide();
         }
     });
@@ -89,9 +95,10 @@ pub fn ask_for_text(label: &str) -> Option<String> {
     while win.shown() {
         app::wait();
     }
-    match input.value() {
-        x if x == "" => None,
-        x => Some(x),
+    let test = *accept.borrow();
+    match test {
+        false => None,
+        true => Some(input.value()),
     }
 }
 pub fn ask_for_record() -> Option<Record> {
@@ -111,15 +118,17 @@ pub fn ask_for_record() -> Option<Record> {
     win.make_modal(true);
     win.show();
 
+    let accept = Rc::new(RefCell::new(false));
+
     butt_accept.set_callback({
-        |x| {
+        let accept = Rc::clone(&accept);
+        move |x| {
+            *accept.borrow_mut() = true;
             x.window().unwrap().hide();
         }
     });
     butt_cancel.set_callback({
-        let mut input = name.clone();
-        move |x| {
-            input.set_value("");
+        |x| {
             x.window().unwrap().hide();
         }
     });
@@ -127,20 +136,21 @@ pub fn ask_for_record() -> Option<Record> {
     while win.shown() {
         app::wait();
     }
-
-    match name.value() {
-        x if x=="" => None,
-        x => {
+    let test = *accept.borrow();
+    match test {
+        false   => None,
+        true    => {
+            let name = name.value();
             let category = category.value();
             if let Ok(value) = value.value().parse() {
                 Some(Record {
-                    name: x,
+                    name,
                     category,
                     value,
                 })
             } else {
                 Some(Record {
-                    name: x,
+                    name,
                     category,
                     value: 0,
                 })
@@ -164,15 +174,17 @@ pub fn ask_for_name() -> Option<Name> {
     win.make_modal(true);
     win.show();
 
+    let accept = Rc::new(RefCell::new(false));
+
     butt_accept.set_callback({
-        |x| {
+        let accept = Rc::clone(&accept);
+        move |x| {
+            *accept.borrow_mut() = true;
             x.window().unwrap().hide();
         }
     });
     butt_cancel.set_callback({
-        let mut input = name.clone();
-        move |x| {
-            input.set_value("");
+        |x| {
             x.window().unwrap().hide();
         }
     });
@@ -180,11 +192,47 @@ pub fn ask_for_name() -> Option<Name> {
     while win.shown() {
         app::wait();
     }
-    match name.value() {
-        x if x == "" => None,
-        x => {
+    let test = *accept.borrow();
+    match test {
+        false => None,
+        true  => {
+            let keyword = name.value();
             let value = value.value();
-            return Some(Name { keyword: x, value });
+            return Some(Name { keyword, value });
         }
     }
+}
+pub fn ask_to_confirm(label: &str) -> bool {
+    let len = i32::max(fltk::draw::width(label) as i32 + 20, 300);
+
+    let mut win = Window::default().with_size(len, 100).with_label(label);
+
+    Frame::new(20, 10, len - 40, 20, None).with_label(label);
+
+    let mut butt_accept = Button::new(len - 100, 60, 80, 30, "Yes");
+    let mut butt_cancel = Button::new(20, 60, 80, 30, "No");
+
+    win.end();
+    win.make_modal(true);
+    win.show();
+
+    let conf = Rc::new(RefCell::new(false));
+
+    butt_accept.set_callback({
+        let conf = Rc::clone(&conf);
+        move |x| {
+            *conf.borrow_mut() = true;
+            x.window().unwrap().hide();
+        }
+    });
+    butt_cancel.set_callback({
+        |x| {
+            x.window().unwrap().hide();
+        }
+    });
+
+    while win.shown() {
+        app::wait();
+    }
+    conf.take()
 }
